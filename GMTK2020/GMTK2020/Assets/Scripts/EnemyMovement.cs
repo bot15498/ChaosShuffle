9e0ff55d1e6bcd7bef80eaeedf6a6a751fbcc4e2
AAYF <samuel.hawaii@gmail.com>
2020-07-10 22:05:42 -1000

    square is kill

--- GMTK2020/GMTK2020/Assets/Scripts/EnemyMovement.cs
+++ GMTK2020/GMTK2020/Assets/Scripts/EnemyMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    public int behaviorValue;
+    public int behaviorValue = 1;
     public float speed;
     Transform playerTransform;
     public float RetreatDistance;
