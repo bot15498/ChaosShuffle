b82469f3ac041898269704a61aad9fed07de459b
skyler <konata.izumi20@gmail.com>
2020-07-12 06:15:39 -0400

    Spinning and touhou mode.

--- GMTK2020/GMTK2020/Assets/Scripts/RotateTowardsMouse.cs
+++ GMTK2020/GMTK2020/Assets/Scripts/RotateTowardsMouse.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RotateTowardsMouse : MonoBehaviour
 {
+    public bool isSpinning = false;
+    public float spinSpeed = 0.8f;
+    private float currAngle = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,18 @@ public class RotateTowardsMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        if(isSpinning)
+        {
+            float anglePerSecond = 360f / spinSpeed;
+            currAngle += anglePerSecond * Time.deltaTime;
+            currAngle = currAngle % 360f;
+            transform.rotation = Quaternion.AngleAxis(currAngle, Vector3.forward);
+        }
+        else
+        {
+            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
--- GMTK2020/GMTK2020/Assets/Scripts/RotateTowardsPlayer.cs
+++ GMTK2020/GMTK2020/Assets/Scripts/RotateTowardsPlayer.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateTowardsPlayer : MonoBehaviour
-{   
+{
+    public bool isSpinning = false;
+    public float spinSpeed = 0.8f;
     public float trackingSpeed;
     private Transform player;
+    private float currAngle = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,19 @@ public class RotateTowardsPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 vectorToTarget = player.position - transform.position;
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * trackingSpeed);
+        if(!isSpinning)
+        {
+            Vector3 vectorToTarget = player.position - transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * trackingSpeed);
+        }
+        else
+        {
+            float anglePerSecond = 360f / spinSpeed;
+            currAngle += anglePerSecond * Time.deltaTime;
+            currAngle = currAngle % 360f;
+            transform.rotation = Quaternion.AngleAxis(currAngle, Vector3.forward);
+        }
     }
 }
